23b500c1fb752eecb49d9e6ddc094c26408f87ed
4507 <4507@TV4507NB-2.tradevanad.intranet.local>
2016-01-16 15:55:51 +0800

    MAP DATA TO JAP

--- src/main/java/org/techzoo/springtiles/controller/SpringTilesController.java
+++ src/main/java/org/techzoo/springtiles/controller/SpringTilesController.java
@@ -1,5 +1,6 @@
 package org.techzoo.springtiles.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,15 +14,33 @@ import org.techzoo.springtiles.vo.Person;
 @Controller
 public class SpringTilesController {
 
+//	@RequestMapping(value="index")
+//	public String index() {
+//		return "index";
+//	}
+	
+
 	@RequestMapping(value="index")
-	public String index() {
-		return "index";
+	public ModelAndView index(Model model) {
+		
+		Map<String, List<Map<String,String>>> data = new HashMap<String, List<Map<String,String>>>();
+		
+		List<Map<String,String>> persons = new ArrayList<Map<String,String>>();
+	
+		Map<String,String> values1 = new HashMap<String,String>();
+		values1.put("name", "AAAA");
+		values1.put("email", "AAAA@AAA");
+		values1.put("age", "AA");
+		persons.add(values1);
+		
+		data.put("persons", persons);
+		
+		return new ModelAndView("personList", data);
 	}
 	
 	@RequestMapping(value="viewPeson")
 	public ModelAndView viewPersons(Model model) {
-		Map<String, List<Person>> persons = 
-				new HashMap<String, List<Person>>();
+		Map<String, List<Person>> persons = new HashMap<String, List<Person>>();
 		persons.put("persons", Person.createPersons());
 		return new ModelAndView("personList", persons);
 	}
